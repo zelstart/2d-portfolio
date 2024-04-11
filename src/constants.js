27b3683951497a062f84ca56bfc173bbf14981ac
Zel Start <134749644+zelstart@users.noreply.github.com>
2024-04-10 22:11:37 -0400

    changed some text

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,8 @@ export const dialogueData = {
     tv: `I like to watch YouTube here!`,
     cert: `I got this certificate from the University of New Hampshire for completing the Full Stack Web Development Bootcamp!`,
     bed: `This is my bed. I'm surprised there isn't a cat on it!`,
-    resume: `Here's a <a href="https://zelstart.netlify.app/resume" target="_blank">link</a> to my resume. Feel free to check it out!`,
-    projects: `If you want to check out some of my work, you can check out my <a href="https://github.com/zelstart?tab=repositories" target="_blank">GitHub</a>!`,
+    resume: `Here's a <a href="https://zelstart.netlify.app/resume" target="_blank">link</a> to my resume.`,
+    projects: `If you want to see some of my work, you can check out my <a href="https://github.com/zelstart?tab=repositories" target="_blank">GitHub</a>!`,
     library: `Here's where I keep all my books. I mostly read fantasy! There's also a few Korean textbooks here. <span id="korean">안녕하세요</span>!`,
     exit: `Thanks for stopping by! If you're ready to go back to my portfolio now, click <a href="https://zelstart.netlify.app">here</a>!`,
 }
